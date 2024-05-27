b0354bf5fe03bf4161a318450dbe7c8a031822b8
Adalina Ma <adalinama@Adalinas-Air.lan>
2024-05-26 17:21:46 -0700

    fix

--- src/components/ScrollyTeller.svelte
+++ src/components/ScrollyTeller.svelte
@@ -81,84 +81,11 @@
       <p>current section: <strong>{index + 1}/{count}</strong></p>
       <progress value={count ? (index + 1) / count : 0} />
       
-<<<<<<< HEAD
       <p>offset in current section</p>
       <progress value={progress || 0} />
       
       <p>total progress</p>
       <progress value={progress || 0} />
-=======
-    }
-  
-    .foreground {
-      width: 60%;
-      margin: 0 auto;
-      height: auto;
-      position: relative;
-      font-size: 30px;
-      font-family: American Typewriter, serif;
-    }
-  
-    .progress-bars {
-      position: absolute;
-      background: #ADD8E6 /*  20% opaque */;
-      opacity: 0.7;
-      visibility: visible;
-    }
-  
-    section {
-      height: 120vh;
-      background-color: rgba(0, 0, 0, 0); /* 20% opaque */
-      /* color: white; */
-      text-align: center;
-      max-width: 1000px; /* adjust at will */
-      color: black;
-      padding: 1em;
-      margin: 0 0 2em 0;
-    }
-  </style>
-  
-  <Scroller
-    top={0.0}
-    bottom={1}
-    threshold={0.5}
-    bind:count
-    bind:index
-    bind:offset
-    bind:progress
-  >
-    <div class="background" slot="background" bind:clientWidth={width} bind:clientHeight={height}>
-      <Map bind:geoJsonToFit {index}/>
-        <div class="progress-bars">
-          <p>current section: <strong>{index + 1}/{count}</strong></p>
-          <progress value={count ? (index + 1) / count : 0} />
-  
-          <p>offset in current section</p>
-          <progress value={progress || 0} />
-  
-          <p>total progress</p>
-          <progress value={progress || 0} />
-        </div>
-      </div>
-  
-    <div class="foreground" slot="foreground">
-      <section>
-        <h1>Flavors of Santa Barbara</h1>
-        <p>A guide to the American Riviera's best culinary experiences</p>
-      </section>
-      <section>
-        You're on a daytrip to Santa Barbara with a group of your friends.
-        After a long morning of exploring the various attractions that the city
-        has to offer, everyone is hungry. However, the area is unfamiliar and your
-        friends are indecisive, so it's difficult to narrow down food options. 
-      </section>
-      <section>
-        <Bar />
-      </section>
-      <section>Fourth section</section>
-      <section>Fifth section</section>
-      <section>Sixth section</section>
->>>>>>> 0a694401df4d17c110cdf5e6a6bb663cbf071687
     </div>
   </div>
   
